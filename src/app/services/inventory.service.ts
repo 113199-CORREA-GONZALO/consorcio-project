06787f0e522ff7c9a4a3842162a41171b19033dc
Valdez Gerardo <114072@tecnicatura.frc.utn.edu.ar>
2024-10-19 13:26:37 -0300

    Update inventory.service.ts

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -8,8 +8,8 @@ import { Inventory, Transaction } from '../models/inventory.model';
   providedIn: 'root'
 })
 export class InventoryService {
-  //private apiArticlesUrl = 'http://localhost:3000/articles'; // URL de la API para los ítems
-  private apiArticlesUrl = 'http://localhost:8080/articles'; // URL de la API para los ítems DEL BACK
+  private apiArticlesUrl = 'http://localhost:3000/articles'; // URL de la API para los ítems
+  //private apiArticlesUrl = 'http://localhost:8080/articles'; // URL de la API para los ítems DEL BACK
   private apiInventoriesUrl = 'http://localhost:3000/inventories'; // URL de la API para los inventarios
   private apiTransactionsUrl = 'http://localhost:3000/transactions'; // URL de la API para las transacciones
 
