ceb7374643e8fff35d5b791ab4643e28b1585b74
Nicolas Nievas <114150@tecnicatura.frc.utn.edu.ar>
2024-10-23 20:03:57 -0300

    eliminacion de imports y redirecc en navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
-import { EmployeesComponent } from './components/employees/employees.component';
 
-import { ArticleComponent } from './components/inventory/inventory_article/inventory_article.component';
-import { TransactionComponent } from './components/inventory/inventory_transaction/inventory_transaction.component';
 import { ProviderListComponent } from './components/provider/provider-list/provider-list.component';
 import { ProviderFormComponent } from './components/provider/provider-form/provider-form.component';
 import { EmployeeListComponent } from './components/employee/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './components/employee/employee-form/employee-form.component';
 
-import { ProvidersComponent } from './components/providers/providers.component';
 import { ArticleComponent } from './components/inventory/inventory-articles/inventory-articles-form/inventory-articles-form.component'
 import { TransactionComponent } from './components/inventory/inventory_transactions/inventory_transaction.component';
 import { InventoryComponent } from './components/inventory/inventory-inventories/inventory-inventories.component';
